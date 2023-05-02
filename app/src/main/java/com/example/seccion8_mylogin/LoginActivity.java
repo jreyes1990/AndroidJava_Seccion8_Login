310bb3a42ba6d9a51259fbd55db9dc7b191cb213
jreyes1990 <jarl1990mt@gmail.com>
2023-05-01 18:43:12 -0600

    Uso de Shared Preferences y SplashScreen.

---
+++ app/src/main/java/com/example/seccion8_mylogin/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.seccion8_mylogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+  }
+}
